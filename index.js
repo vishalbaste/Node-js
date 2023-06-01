061944b7ac431a6949016c5cf2457dc8f8a57980
vishalbaste <vishalbaste726@gmail.com>
2023-06-01 20:10:32 +0530

    Export Modules

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const person = require('./secound');
-const obj = new person();
-console.log(obj.name);
+const http = require('http');
+
+const server = http.createServer((req,responce)=>
+{
+    responce.writeHead(200,{'Content-Type' : 'text/html'})
+    const obj = new person();
+    responce.write(obj.name);
+    responce.end();
+    console.log('Server Lisaning on 8090');
+});
+
+server.listen(8090);
